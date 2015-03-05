bf0d25b8035ef6f2a0b661796fd8a385ac692714
Andy Gocke <angocke@microsoft.com>
2015-03-05 13:26:56 -0800

    Move csc2/vbc2 logic into csc/vbc behind '/shared' flag
    This change adds support for the '/shared' flag to csc/vbc, which indicates
    to the command line compiler that it should attempt to use the compiler server.
    
    This change doesn't yet delete the csc2 and vbc2 binaries to preserve compatibility
    for our partners until they have a change to switch to the new API.

--- src/Compilers/VisualBasic/vbc/Program.cs
+++ src/Compilers/VisualBasic/vbc/Program.cs
@@ -1,13 +1,13 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using Microsoft.CodeAnalysis.BuildTasks;
-using static Microsoft.CodeAnalysis.CompilerServer.BuildProtocolConstants.RequestLanguage;
+using static Microsoft.CodeAnalysis.CompilerServer.BuildProtocolConstants;
 
 namespace Microsoft.CodeAnalysis.VisualBasic.CommandLine
 {
-    public class Vbc2
+    public class Program
     {
         public static int Main(string[] args)
-            => BuildClient.RunWithConsoleOutput(args, VisualBasicCompile, Program.Main);
+            => BuildClient.RunWithConsoleOutput(args, RequestLanguage.VisualBasicCompile, Vbc.Run);
     }
 }
